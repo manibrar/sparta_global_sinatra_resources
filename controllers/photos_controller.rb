f039b7ff33276935987e104eb7f81285bfd02842
manibrar <manvirbrar1988@gmail.com>
2018-06-27 23:58:44 +0100

    dynamic shirt pages

--- controllers/photos_controller.rb
+++ controllers/photos_controller.rb
@@ -6,13 +6,48 @@ class PhotosController < Sinatra::Base
   register Sinatra::Reloader
 end
 
+shirts = [
+  {
+  id:0,
+  title: "Extra small shirt",
+  size: "XS",
+  colour: "White",
+  material:"Plaid"
+},
+{
+  id:1,
+  title: "Small shirt",
+  size: "S",
+  colour: "Blue",
+  material:"Denim"
+},
+{
+  id:2,
+  title: "Medium shirt",
+  size: "M",
+  colour: "Red",
+  material:"Silk"
+},
+{
+  id:3,
+  title: "Large shirt",
+  size: "xs",
+  colour: "Black",
+  material:"Nylon"
+}
+]
 
 
 get '/' do
-  "Hello world, "
-
+  
+erb :'./photos/index'
 end
 
-get '/:id'
-  params[:id]
+get '/:id' do
+  id = params[:id].to_i
+  @shirt = shirts[id]
+  "This is a #{@shirt[:title]}, tag size is #{@shirt[:size]}. The colour is #{@shirt[:colour]} and it's made from #{@shirt[:material]} "
+  erb :'./photos/show'
+end
+
 end
